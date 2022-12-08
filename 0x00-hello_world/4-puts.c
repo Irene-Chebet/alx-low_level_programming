c13ff5ac3c05dd616d3000c0215579dbaf3d8280
Irene-Chebet <ichebet78@gmail.com>
2022-12-08 12:22:21 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #!/bin/bash
 #include <stdio.h>
 
-
-
 /**
  *
  * *main - Entry point
@@ -17,8 +15,8 @@ int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+					return (0);
 
 }
