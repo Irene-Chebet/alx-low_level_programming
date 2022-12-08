4ab9f58e10c22a06c12e96abd41ca55764206f0b
Irene-Chebet <ichebet78@gmail.com>
2022-12-08 14:10:36 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,26 +1,13 @@
-#!/bin/bash
-#include <stdio.h>
-
 #include <unistd.h>
-
-
-
 /**
+ *main-program entry point.
  *
- * *main - Entry point
- *
- * *Return: Always 1
- *
- * */
-
-
-
+ *Return: 1 on success.
+ */
 int main(void)
-
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-			return (1);
-
+	return (1);
 }
