0be0d599c265015b3020eaefc0970d7a0a8c103d
Irene-Chebet <ichebet78@gmail.com>
2022-12-13 21:28:12 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
-/**
-*main - print _putchar
-*is written by mbah
-*Return: 0
-*/
 
+/**
+ * main - prints _putchar and a new line
+ * Return: Always 0
+ */
 int main(void)
 {
 	_putchar('_');
@@ -16,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
