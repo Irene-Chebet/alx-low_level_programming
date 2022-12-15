287d7c8d96cf1e4b961e1c9167afe0b8774c8944
Irene-Chebet <ichebet78@gmail.com>
2022-12-15 11:08:27 -0800

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,38 +1,60 @@
 #include "main.h"
-/**
- * print_number - prints a number
- * @n: Input number
- */
 
+/**
+ * print_number - prints a number using _putchar
+ * @n: number to print
+ *
+ * Return: void
+ */
 void print_number(int n)
 {
-	long len, res, i, temp, expo;
+	unsigned int un = 0;
 
-	res = n;
-	expo = len =  1;
-/*Check negatives*/
-	if (res < 0)
+	if (n < 0)
 	{
-		res *= -1;
-		_putchar('-');
+		if (n < 1000000000)
+			n = -n;
+		un = n;
+		_putchar(45);
+		num_to_char(n);
 	}
+	else
+	{
+		un = n;
+		num_to_char(un);
+	}
+}
 
-/**/
-	temp = res;
-	while (temp >= 10)
-	{
-		len++;
-		temp /= 10;
-	}
+/**
+ * num_to_char - transforms a number with 1 or more digits into a char
+ * @n: number to print
+ *
+ * Return: void
+ */
+void num_to_char(unsigned int n)
+{
+	unsigned int d = 10;
 
-/*Create Exponent*/
-	for (i = 1; i < len; i++)
-		expo *= 10;
-/*Main */
-	while (expo > 1)
+	if (n < d)
 	{
-		_putchar((res / expo) % 10 + '0');
-		expo /= 10;
+		_putchar('0' + n);
 	}
-	_putchar(res % 10 + '0');
+	else
+	{
+		while (n >= d)
+		{
+			d *= 10;
+			if (d == 1000000000)
+				break;
+		}
+		if (!(d == 1000000000) || n == 123456789)
+			d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
+	}
 }
