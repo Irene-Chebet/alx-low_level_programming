38183280001e9f47e35a67d47cb7e301c3ac2365
Irene-Chebet <ichebet78@gmail.com>
2023-02-01 10:27:03 -0800

    1. List length task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,21 @@
 #include "lists.h"
+
 /**
- * listint_len - the length of linked list
- * @h: the list
- * Return: the number of node
- */
+ * listint_len - Calculate the number of elements.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
 size_t listint_len(const listint_t *h)
 {
-	size_t node_num = 0;
+	const listint_t *tp;
+	unsigned int cnr = 0;
 
-	if (h == NULL)
-		return (0);
-	if (h->next != NULL)
+	tp = h;
+	while (tp)
 	{
-		node_num++;
-		h = h->next;
+		cnr++;
+		tp = tp->next;
 	}
-	return (node_num + 1);
+	return (cnr);
 }
