48d322ce087c4478ff77c08108779d03959483ff
Irene-Chebet <ichebet78@gmail.com>
2023-04-07 05:37:07 -0700

    >>> ht[betty]

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,29 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - retrieves a value associated with a key
- * @ht: the hash table
- * @key: the key
- * Return: the value or null
+ * hash_table_get - Retrieve the value associated with
+ *                  a key in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to get the value of.
+ *
+ * Return: If the key cannot be matched - NULL.
+ *         Otherwise - the value associated with key in ht.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *node;
 	unsigned long int index;
-	hash_node_t *elem;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	if (index >= ht->size)
-	{
 		return (NULL);
-	}
-	elem = ht->array[index];
-	while (elem && strcmp(elem->key, key) != 0)
-	{
-		elem = elem->next;
-	}
-	if (elem == NULL)
-	{
-		return (NULL);
-	}
-	return (elem->value);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
 }
